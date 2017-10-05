2fecffac19e0942fda65e757f6189359638e0690
jnyyssol <jnyyssol@ts137-pc21.student.yo.oulu.fi>
2017-10-05 14:52:36 +0300

    not finished

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -7,4 +7,105 @@ public class SudokuVerifier {
 		
 		//check
 	}
+	
+	public int checkLengthDigit(String canditateSolution)
+	{
+		if (canditateSolution.length() > 81)
+			return -1;
+		
+		for (char c : canditateSolution.toCharArray()) {
+			if (!Character.isDigit(c))
+				return -1;
+		}
+		
+		return 0;
+	}
+	
+	public int[][] formGrids(String canditateSolution) {
+		
+		int grid[][] = new int[9][9];
+		int rowno = 0;
+		
+		for(int i = 0; i < 81; i++) {
+			if (i % 9 == 0 && rowno < 8) 
+			{
+				rowno++;
+			}
+			grid[i % 9][rowno] = Character.getNumericValue(canditateSolution.toCharArray()[i]);
+		}
+		return grid;
+	}
+	
+	public int rowCheck(int[][] theGrid) {
+		
+		for(int row=0; row < 9; row++) {
+			for(int col=0; col < 9; col++) {
+				for(int spot=0; spot < 9; spot++) {
+					if(theGrid[row][col] == theGrid[row][spot] && spot != col) {
+						return -3;
+					}
+				}
+			}
+		}
+		return 0;
+	}
+	
+	public int columnCheck(int[][] theGrid) {
+		
+		for(int col=0; col < 9; col++) {
+			for(int row=0; row < 9; row++) {
+				for(int spot=0; spot < 9; spot++) {
+					if(theGrid[row][col] == theGrid[spot][col] && spot != row) {
+						return -4;
+					}
+				}
+			}
+		}
+		return 0;
+	}
+	
+	//doesn't work
+	
+	public int subGridCheck(int[][] theGrid) {
+		
+		int skip = 0;
+		int[] ingrid = new int[9];
+		int realrow = 0;
+		int ingspot = 0;
+		
+		
+		for(int i=0; i<9; i++) {
+			for(int j=0; j<27; j++) {
+				skip++;
+				if (skip % 3 == 0) {
+					j = j+6;
+				}
+				
+				ingrid[ingspot] = theGrid[j % 3][realrow];
+				ingspot++;
+				
+				if (skip % 3 == 0) {
+					skip=0;
+					realrow++;
+				}
+				
+			}
+			for (int j=0; j<9; j++) {
+			      for (int k=0; k<9; k++) {
+			    	  if (k!=j && ingrid[k] == ingrid[j]) {
+			    		  return -2; 
+			    	  }
+			      }
+			}
+			ingspot = 0;
+		}
+		
+		return 0;
+	}
 }
+
+
+
+
+
+
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -7,9 +7,71 @@ public class SudokuVerifierTest {
 // A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 
-	@Test
-	public void testVerify() {
-		fail("Not yet implemented");
+	@Test 
+	public void testSudokuVerifier_NumberCount()  {
+		SudokuVerifier verify = new SudokuVerifier();
+		
+		String testr = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		
+		assertEquals(0, verify.checkLengthDigit(testr));
 	}
-
+	
+	@Test 
+	public void testSudokuVerifier_noDigitFail()  {
+		SudokuVerifier verify = new SudokuVerifier();
+		
+		String testr = "417369825632158947958724316825437169791586432346912758289643571573295684164875jjj";
+		
+		assertEquals(-1, verify.checkLengthDigit(testr));
+	}
+	
+	@Test 
+	public void testSudokuVerifier_testGridding()  {
+		SudokuVerifier verify = new SudokuVerifier();
+		
+		String testr = "417369825632158947958724316825437169791586432346912758289643571573295684164875293";
+		
+		int[][] grid = verify.formGrids(testr);
+		
+		assertEquals(3, grid[8][8]);
+	}
+	
+	@Test 
+	public void testSudokuVerifier_testRow()  {
+		SudokuVerifier verify = new SudokuVerifier();
+		
+		String testr = "417369825632158947958724316825437169791586432346912758289643571573295684164875293";
+		
+		int[][] grid = verify.formGrids(testr);
+		
+		assertEquals(0, verify.rowCheck(grid));
+	}
+	
+	@Test 
+	public void testSudokuVerifier_testColumnFail()  {
+		SudokuVerifier verify = new SudokuVerifier();
+		
+		String testr = "417369825632158947958724316825437169791586432346912758289643571573295684164875293";
+		
+		int[][] grid = verify.formGrids(testr);
+		
+		grid[8][7] = 1;
+		grid[8][8] = 1;
+		
+		assertEquals(-3, verify.rowCheck(grid));
+	}
+	
+	@Test 
+	public void testSudokuVerifier_subGridtest()  {
+		SudokuVerifier verify = new SudokuVerifier();
+		
+		String testr = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		
+		int[][] grid = verify.formGrids(testr);
+		
+		
+		assertEquals(0, verify.subGridCheck(grid));
+	}
+	
+	
 }
